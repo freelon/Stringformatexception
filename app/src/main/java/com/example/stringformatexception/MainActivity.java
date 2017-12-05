aa5abda79190e85ee13ca641b48e54d4d479bc0b
Johannes <jschaef@mail.uni-paderborn.de>
2017-12-05 08:19:36 +0100

    failing databinding

--- app/src/main/java/com/example/stringformatexception/MainActivity.java
+++ app/src/main/java/com/example/stringformatexception/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.stringformatexception;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.stringformatexception.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainViewModel model = new MainViewModel();
+        binding.setModel(model);
     }
 }
---
+++ app/src/main/java/com/example/stringformatexception/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.stringformatexception;
+
+import android.databinding.ObservableField;
+
+public class MainViewModel {
+    public final ObservableField<Double> value = new ObservableField<>(1.0);
+}
